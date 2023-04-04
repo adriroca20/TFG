286a44502fedda5ea79afa3407f6c4a213f3ccbb
Rocafull Berbel <adrian.rocafull-berbel@capgemini.com>
2023-04-04 13:27:12 +0200

    Zoom en galeria

--- angular-ecommerce/src/app/components/image-gallery/image-gallery.component.ts
+++ angular-ecommerce/src/app/components/image-gallery/image-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild} from '@angular/core';
+import { Component, Input, HostListener} from '@angular/core';
 import { imageGallery } from 'src/app/interfaces/Interfaces';
 
 @Component({
@@ -11,8 +11,33 @@ export class ImageGalleryComponent {
   setData!:imageGallery;
   images:Array<string>=[];
   currentImage:number=0;
+  mainImage!:any
+  @HostListener('mousemove',['$event'])
+  zoomImage(event:any){
+    let areaImage:any= event.target
+    if(areaImage.className == "main-image"){
+    let image:any= event.target.firstChild
+    let clientX:any = event.clientX - areaImage.offsetLeft;
+    let clientY:any = event.clientY - areaImage.offsetTop;
+    let mWidth= areaImage.offsetWidth
+    let mHeight= areaImage.offsetHeight
+    clientX= clientX/mWidth * 100
+    clientY= clientY/mHeight * 100
+    let transformation:string= 'translate(-' + clientX + '%,-'+ clientY+ '%) scale(2)'
+    image.style.transform=transformation
+    }
+  }
+  @HostListener('mouseleave',['$event'])
+  restoreImage(event:any){
+    if(event.target.className == "main-image"){
+      let image:any= event.target.firstChild
+      image.style.transform="translate(-50%, -50%) scale(1)"
+    }
+  }
   ngOnInit(){
-    console.log(this.setData)
+    this.mainImage=document.querySelectorAll(".main-image")[0]
+    this.mainImage.addEventListener
+    console.log(this.mainImage)
     this.setData.images.forEach(image => {
       this.images.push(image)
     });
